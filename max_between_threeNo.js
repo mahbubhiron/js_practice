aa11bdc54d7f6e50746f41d954a32da30cddcaea
Md. Mahbub hiron <mhalom19@gmail.com>
2022-01-29 13:40:18 +0600

    max

--- max_between_threeNo.js
+++ max_between_threeNo.js
@@ -50,3 +50,23 @@ function second() {
     return sMax;
 }
 console.log("second largest number using library function", second());
+
+var arr =[
+    [1,0,0],
+    [0,1,0],
+    [0,0,1]
+    ];
+    var f=0;
+    // console.log(arr[0][0],arr[0][1]);
+    for(let i=0;i<arr.length;i++){
+        for(let j=0;j<arr.length;j++){
+        // console.log(arr[i][j]);
+        if(i==j&&arr[i][j]!=1||i!=j&&arr[i][j]!=0){
+            f=1;
+            break;
+         }
+        }
+    }
+    if(f==0){
+        console.log("unit matrix");
+    }
